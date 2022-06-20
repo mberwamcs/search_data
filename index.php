4b2baba60ef5ebbda03f0d68034183468ddea7db
mberwamcs <hmberwa1@gmail.com>
2022-06-20 01:40:47 -0700

    search with a keyword

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+       // include('search.php');
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,9 +11,42 @@
 </head>
 <body>
     <h3>Search in the Database</h3>
-    <form action="" metihod="POST">
-        <input type="text" placeholder="Search ..."><br>
+    <form action="" method="POST">
+        <input type="text" name="searchteam" placeholder="Search ..."><br><br>
         <input type="submit" name="search" id="search" value="Search">
     </form>
+    <?php
+       $db =  mysqli_connect("localhost", "root", "", "gesyclean"); 
+       //mysqli_select_db("gesyclean");
+
+       if(!$db){
+        echo "connection error";
+       } else{ }
+      //php 'trim()' -removes spaces
+      //'sql mysqli_real_escape_string()' -Escapes special characters in a string for use in an SQL statement
+      if(isset($_POST['search'])){
+      $search =trim($_POST['searchteam']); 
+      //
+      
+      $query = "SELECT * FROM timesheet WHERE `acc_name` LIKE '%$search%'";
+      $find_data = mysqli_query($db, $query);
+
+     // if(!$find_data){
+       // echo "!!!!!!!";
+        //($find_data);
+     // }
+      //else{
+       while($row = mysqli_fetch_assoc($find_data)){
+
+        $name = $row['acc_name'];
+
+
+        echo "$name<br/>";
+       }
+   // }
+}else {
+    echo "Enter a keyword to search";
+}
+    ?>
 </body>
 </html>
