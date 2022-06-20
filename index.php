a16674327e794adc2728a7dd3d8eeffde14d64ed
mberwamcs <hmberwa1@gmail.com>
2022-06-19 19:14:04 -0700

    index.php is added

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>search by keyword</title>
+</head>
+<body>
+    <h3>Search in the Database</h3>
+    <form action="" method="POST">
+        <input type="text" placeholder="Search ...">
+        <input type="submit" name="search" value="Search">
+    </form>
+</body>
+</html>
